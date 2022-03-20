2dc7f773333a81a643949c267f84585075ad5002
Cody Partington <codythatsme@gmail.com>
2022-03-20 16:36:23 +1000

    Updated some language in code

--- main.rb
+++ main.rb
@@ -3,46 +3,43 @@ require 'json'
 players = JSON.load_file('scoreboard.json')
 last_winner = ""
 
-def leaderboard_display(arr, choice)
+def leaderboard_display(arr, menu)
     sorted = arr.clone.sort_by { |player| player[4] }
     sorted.reverse!.each_with_index do |player, i|
-        if choice == "summary"
+        if menu == "summary"
         puts "#{i + 1}. #{player[0]} Score: #{player[4]}"
-        elsif choice == "details"
+        elsif menu == "details"
             puts "#{i + 1}. #{player[0]} With #{player[1]} First place wins, #{player[2]} runner up placements and #{player[3]} 3rd placements."
         end
     end
 end
 
 def score_adder(first, second, third, arr)
-  contains_first = arr.any? { |s| s.include?(first) }
-  if contains_first == true
+    if arr.any? { |s| s.include?(first) }
     arr.each_with_index do |_player, i|
         arr[i][1] += 1 if arr[i].include?(first)
     end
-  else
+    else
         arr.push([first, 1, 0, 0, 0])
-  end
-  contains_second = arr.any? { |s| s.include?(second) }
-  if contains_second == true
+    end
+    if arr.any? { |s| s.include?(second) }
     arr.each_with_index do |_player, i|
         arr[i][2] += 1 if arr[i].include?(second)
     end
-  else
+    else
         arr.push([second, 0, 1, 0, 0])
-  end
-  contains_third = arr.any? { |s| s.include?(third) }
-  if contains_third == true
+    end
+    if arr.any? { |s| s.include?(third) }
     arr.each_with_index do |_player, i|
         arr[i][3] += 1 if arr[i].include?(third)
     end
-  else
+    else
         arr.push([third, 0, 0, 1, 0])
-  end
+    end
 
-  arr.each_with_index do |_player, i|
-          arr[i][4] = ((arr[i][1]) * 2) + arr[i][2] + arr[i][3]
-  end
+    arr.each_with_index do |_player, i|
+            arr[i][4] = ((arr[i][1]) * 2) + arr[i][2] + arr[i][3]
+    end
 end
 
 quit = false
@@ -64,11 +61,13 @@ while quit == false
             puts "3. Back"
             choice = gets.chomp.to_i
             if choice == 1
+                menu = "summary"
                 system "clear"
-                leaderboard_display(players, "summary")
+                leaderboard_display(players, menu)
             elsif choice == 2
+                menu = "details"
                 system "clear"
-                leaderboard_display(players, "details")
+                leaderboard_display(players, menu)
             elsif choice == 3
                 leaderboard_menu = false
             else
@@ -81,13 +80,13 @@ while quit == false
         while admin_menu == true
         puts "Please enter the password to modify the leaderboard, or type back to navigate back."
         pw = gets.chomp
-        if pw == "p"
+        if pw == "plaintextpasswordlol"
             puts "Please enter the name of todays champion"
-            champ = gets.chomp
+            champ = gets.chomp.capitalize
             puts "Please enter todays runner up"
-            runner_up = gets.chomp
+            runner_up = gets.chomp.capitalize
             puts "Enter today's third place player"
-            third = gets.chomp
+            third = gets.chomp.capitalize
             score_adder(champ, runner_up, third, players)
             last_winner = champ
             File.write('scoreboard.json', JSON.pretty_generate(players))
